90018f33ad4552ebae7fcc0044421740d1740c26
Alex420s <edgaring1998@gmail.com>
2023-07-24 14:21:08 -0600

    deploy?

---
+++ build_files.sh
@@ -0,0 +1,7 @@
+python3.9 -m venv venv
+source venv/bin/activate
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput --clear
--- myapp/settings.py
+++ myapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-pe%i)*hsgym9bs#@o$udrqokfbn2wv!#us^39i)^5!w$e-x2*^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
